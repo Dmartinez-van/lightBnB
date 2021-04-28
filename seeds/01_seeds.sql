0b6ac889eb07504e4325b9fbf2182787d6126393
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-04-28 14:21:17 -0700

    Populate with seed 02 data

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -4,7 +4,7 @@ VALUES
 ('bob', 'bob@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
 ('sally', 'sally@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
-INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, is_acitive)
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
 VALUES 
 (1, 'House', 'description', '/images/thumbs/1.png', '/images/covers/1.png', 500, 2, 2, 5, 'Canada', '2883 Shuttle St', 'Abbotsford', 'BC', 'v8c 3d2', TRUE),
 (1, 'Shed', 'description', '/images/thumbs/2.png', '/images/covers/2.png', 1500, 2, 5, 3, 'Canada', '2883 Tutrle Ave', 'Calgary', 'AB', 'v1c 6g2', TRUE),
