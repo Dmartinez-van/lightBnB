4e0482b714cafcbfc4cd0dd8063db7ece098b856
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-04-29 15:05:04 -0700

    Update addProperty function to add to DB

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -71,9 +71,6 @@ const addUser =  function(user) {
   // users[userId] = user;
   // return Promise.resolve(user);
   
-  // const name = user.name;
-  // const email = user.email;
-  // const password = user.password;
   
   return pool
     .query(`
@@ -176,8 +173,6 @@ const getAllProperties = (options, limit = 10) => {
                  `;
   }
 
-
-
   queryParams.push(limit);
   if (options.minimum_rating) {
     queryString += `
@@ -192,7 +187,7 @@ const getAllProperties = (options, limit = 10) => {
     `;
   }
 
-  console.log(queryString, queryParams);
+  // console.log(queryString, queryParams);
 
   return pool.query(queryString, queryParams).then(res => res.rows);
   
@@ -218,9 +213,22 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
+  console.log(typeof property.owner_id);
+  return pool
+    .query(`
+    INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night,
+        parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;
+    `, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url,
+      Number(property.cost_per_night), Number(property.parking_spaces), Number(property.number_of_bathrooms), Number(property.number_of_bedrooms),
+      property.country, property.street, property.city, property.province, property.post_code])
+    .then(result => result.rows[0])
+    .catch(err => err);
+
 };
 exports.addProperty = addProperty;
